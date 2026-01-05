18eb6634ae6bac412a1a17dbe625b4cdc3c7289f
pen091 <princeekenendioziri@gmail.com>
2026-01-05 04:56:17 +0000

    remote access

--- client.c
+++ client.c
@@ -14,8 +14,7 @@
 #include <errno.h>
 
 #define BUFFER_SIZE 2048
-#define SERVER_IP "https://motorola-mailed-vocational-point.trycloudfl
-are.com                                |             "
+#define SERVER_IP "https://motorola-mailed-vocational-point.trycloudflare.com"
 #define SERVER_PORT 8080
 
 int client_socket;
